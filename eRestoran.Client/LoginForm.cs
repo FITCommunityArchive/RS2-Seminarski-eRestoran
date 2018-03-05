154f29f79e446b0c0a2806e6593752727fa90b76
Shekey <ajdin_sah@hotmail.com>
2018-03-05 13:39:55 +0100

    testpromjena

--- eRestoran.Client/LoginForm.cs
+++ eRestoran.Client/LoginForm.cs
@@ -1,5 +1,4 @@
-﻿using API.Models;
-using ggg.Util;
+﻿
 using System;
 using System.Collections.Generic;
 using System.ComponentModel;
@@ -61,17 +60,7 @@ namespace ggg
             //}
         }
 
-        private void linkLabel2_LinkClicked(object sender, LinkLabelLinkClickedEventArgs e)
-        {
-            //enter your code for forget password case
-            MessageBox.Show("Under development");
-        }
-
-        private void linkLabel1_LinkClicked(object sender, LinkLabelLinkClickedEventArgs e)
-        {
-            //Enter your code for registration form of your choice
-            MessageBox.Show("Under development");
-        }
+      
 
        
     }
