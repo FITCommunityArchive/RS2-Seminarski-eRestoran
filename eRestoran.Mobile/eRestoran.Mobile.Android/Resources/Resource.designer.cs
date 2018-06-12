f4b2ea8d0d33b02f41e11f44d312ebf71d596323
Mirza <mirza.ho@hotmail.com>
2018-06-12 22:55:02 +0200

    - Login added

--- eRestoran.Mobile/eRestoran.Mobile.Android/Resources/Resource.designer.cs
+++ eRestoran.Mobile/eRestoran.Mobile.Android/Resources/Resource.designer.cs
@@ -2790,8 +2790,8 @@ namespace eRestoran.Mobile.Droid
 		public partial class Id
 		{
 			
-			// aapt resource value: 0x7f09009e
-			public const int action0 = 2131296414;
+			// aapt resource value: 0x7f0900a4
+			public const int action0 = 2131296420;
 			
 			// aapt resource value: 0x7f090064
 			public const int action_bar = 2131296356;
@@ -2814,17 +2814,17 @@ namespace eRestoran.Mobile.Droid
 			// aapt resource value: 0x7f090041
 			public const int action_bar_title = 2131296321;
 			
-			// aapt resource value: 0x7f09009b
-			public const int action_container = 2131296411;
+			// aapt resource value: 0x7f0900a1
+			public const int action_container = 2131296417;
 			
 			// aapt resource value: 0x7f090065
 			public const int action_context_bar = 2131296357;
 			
-			// aapt resource value: 0x7f0900a2
-			public const int action_divider = 2131296418;
+			// aapt resource value: 0x7f0900a8
+			public const int action_divider = 2131296424;
 			
-			// aapt resource value: 0x7f09009c
-			public const int action_image = 2131296412;
+			// aapt resource value: 0x7f0900a2
+			public const int action_image = 2131296418;
 			
 			// aapt resource value: 0x7f090003
 			public const int action_menu_divider = 2131296259;
@@ -2841,11 +2841,11 @@ namespace eRestoran.Mobile.Droid
 			// aapt resource value: 0x7f090043
 			public const int action_mode_close_button = 2131296323;
 			
-			// aapt resource value: 0x7f09009d
-			public const int action_text = 2131296413;
+			// aapt resource value: 0x7f0900a3
+			public const int action_text = 2131296419;
 			
-			// aapt resource value: 0x7f0900ab
-			public const int actions = 2131296427;
+			// aapt resource value: 0x7f0900b1
+			public const int actions = 2131296433;
 			
 			// aapt resource value: 0x7f090044
 			public const int activity_chooser_view_content = 2131296324;
@@ -2871,11 +2871,14 @@ namespace eRestoran.Mobile.Droid
 			// aapt resource value: 0x7f090028
 			public const int bottom = 2131296296;
 			
+			// aapt resource value: 0x7f090086
+			public const int btnLogin = 2131296390;
+			
 			// aapt resource value: 0x7f09004b
 			public const int buttonPanel = 2131296331;
 			
-			// aapt resource value: 0x7f09009f
-			public const int cancel_action = 2131296415;
+			// aapt resource value: 0x7f0900a5
+			public const int cancel_action = 2131296421;
 			
 			// aapt resource value: 0x7f090030
 			public const int center = 2131296304;
@@ -2889,8 +2892,8 @@ namespace eRestoran.Mobile.Droid
 			// aapt resource value: 0x7f09005b
 			public const int checkbox = 2131296347;
 			
-			// aapt resource value: 0x7f0900a7
-			public const int chronometer = 2131296423;
+			// aapt resource value: 0x7f0900ad
+			public const int chronometer = 2131296429;
 			
 			// aapt resource value: 0x7f090039
 			public const int clip_horizontal = 2131296313;
@@ -2946,8 +2949,8 @@ namespace eRestoran.Mobile.Droid
 			// aapt resource value: 0x7f090021
 			public const int end = 2131296289;
 			
-			// aapt resource value: 0x7f0900b1
-			public const int end_padder = 2131296433;
+			// aapt resource value: 0x7f0900b7
+			public const int end_padder = 2131296439;
 			
 			// aapt resource value: 0x7f09002a
 			public const int enterAlways = 2131296298;
@@ -2985,8 +2988,8 @@ namespace eRestoran.Mobile.Droid
 			// aapt resource value: 0x7f090049
 			public const int icon = 2131296329;
 			
-			// aapt resource value: 0x7f0900ac
-			public const int icon_group = 2131296428;
+			// aapt resource value: 0x7f0900b2
+			public const int icon_group = 2131296434;
 			
 			// aapt resource value: 0x7f090025
 			public const int ifRoom = 2131296293;
@@ -2994,8 +2997,8 @@ namespace eRestoran.Mobile.Droid
 			// aapt resource value: 0x7f090046
 			public const int image = 2131296326;
 			
-			// aapt resource value: 0x7f0900a8
-			public const int info = 2131296424;
+			// aapt resource value: 0x7f0900ae
+			public const int info = 2131296430;
 			
 			// aapt resource value: 0x7f090000
 			public const int item_touch_helper_previous_elevation = 2131296256;
@@ -3006,11 +3009,11 @@ namespace eRestoran.Mobile.Droid
 			// aapt resource value: 0x7f090034
 			public const int left = 2131296308;
 			
-			// aapt resource value: 0x7f0900ad
-			public const int line1 = 2131296429;
+			// aapt resource value: 0x7f0900b3
+			public const int line1 = 2131296435;
 			
-			// aapt resource value: 0x7f0900af
-			public const int line3 = 2131296431;
+			// aapt resource value: 0x7f0900b5
+			public const int line3 = 2131296437;
 			
 			// aapt resource value: 0x7f09000f
 			public const int listMode = 2131296271;
@@ -3018,11 +3021,11 @@ namespace eRestoran.Mobile.Droid
 			// aapt resource value: 0x7f090048
 			public const int list_item = 2131296328;
 			
-			// aapt resource value: 0x7f0900b5
-			public const int masked = 2131296437;
+			// aapt resource value: 0x7f0900bb
+			public const int masked = 2131296443;
 			
-			// aapt resource value: 0x7f0900a1
-			public const int media_actions = 2131296417;
+			// aapt resource value: 0x7f0900a7
+			public const int media_actions = 2131296423;
 			
 			// aapt resource value: 0x7f090022
 			public const int middle = 2131296290;
@@ -3030,80 +3033,80 @@ namespace eRestoran.Mobile.Droid
 			// aapt resource value: 0x7f09003e
 			public const int mini = 2131296318;
 			
-			// aapt resource value: 0x7f09008d
-			public const int mr_art = 2131296397;
-			
-			// aapt resource value: 0x7f090082
-			public const int mr_chooser_list = 2131296386;
-			
-			// aapt resource value: 0x7f090085
-			public const int mr_chooser_route_desc = 2131296389;
-			
-			// aapt resource value: 0x7f090083
-			public const int mr_chooser_route_icon = 2131296387;
-			
-			// aapt resource value: 0x7f090084
-			public const int mr_chooser_route_name = 2131296388;
-			
-			// aapt resource value: 0x7f090081
-			public const int mr_chooser_title = 2131296385;
-			
-			// aapt resource value: 0x7f09008a
-			public const int mr_close = 2131296394;
-			
-			// aapt resource value: 0x7f090090
-			public const int mr_control_divider = 2131296400;
-			
-			// aapt resource value: 0x7f090096
-			public const int mr_control_playback_ctrl = 2131296406;
-			
-			// aapt resource value: 0x7f090099
-			public const int mr_control_subtitle = 2131296409;
-			
-			// aapt resource value: 0x7f090098
-			public const int mr_control_title = 2131296408;
-			
-			// aapt resource value: 0x7f090097
-			public const int mr_control_title_container = 2131296407;
-			
-			// aapt resource value: 0x7f09008b
-			public const int mr_custom_control = 2131296395;
-			
-			// aapt resource value: 0x7f09008c
-			public const int mr_default_control = 2131296396;
-			
-			// aapt resource value: 0x7f090087
-			public const int mr_dialog_area = 2131296391;
-			
-			// aapt resource value: 0x7f090086
-			public const int mr_expandable_area = 2131296390;
-			
-			// aapt resource value: 0x7f09009a
-			public const int mr_group_expand_collapse = 2131296410;
-			
-			// aapt resource value: 0x7f09008e
-			public const int mr_media_main_control = 2131296398;
-			
-			// aapt resource value: 0x7f090089
-			public const int mr_name = 2131296393;
-			
-			// aapt resource value: 0x7f09008f
-			public const int mr_playback_control = 2131296399;
+			// aapt resource value: 0x7f090093
+			public const int mr_art = 2131296403;
 			
 			// aapt resource value: 0x7f090088
-			public const int mr_title_bar = 2131296392;
+			public const int mr_chooser_list = 2131296392;
+			
+			// aapt resource value: 0x7f09008b
+			public const int mr_chooser_route_desc = 2131296395;
+			
+			// aapt resource value: 0x7f090089
+			public const int mr_chooser_route_icon = 2131296393;
+			
+			// aapt resource value: 0x7f09008a
+			public const int mr_chooser_route_name = 2131296394;
+			
+			// aapt resource value: 0x7f090087
+			public const int mr_chooser_title = 2131296391;
+			
+			// aapt resource value: 0x7f090090
+			public const int mr_close = 2131296400;
+			
+			// aapt resource value: 0x7f090096
+			public const int mr_control_divider = 2131296406;
+			
+			// aapt resource value: 0x7f09009c
+			public const int mr_control_playback_ctrl = 2131296412;
+			
+			// aapt resource value: 0x7f09009f
+			public const int mr_control_subtitle = 2131296415;
+			
+			// aapt resource value: 0x7f09009e
+			public const int mr_control_title = 2131296414;
+			
+			// aapt resource value: 0x7f09009d
+			public const int mr_control_title_container = 2131296413;
 			
 			// aapt resource value: 0x7f090091
-			public const int mr_volume_control = 2131296401;
+			public const int mr_custom_control = 2131296401;
 			
 			// aapt resource value: 0x7f090092
-			public const int mr_volume_group_list = 2131296402;
+			public const int mr_default_control = 2131296402;
+			
+			// aapt resource value: 0x7f09008d
+			public const int mr_dialog_area = 2131296397;
+			
+			// aapt resource value: 0x7f09008c
+			public const int mr_expandable_area = 2131296396;
+			
+			// aapt resource value: 0x7f0900a0
+			public const int mr_group_expand_collapse = 2131296416;
 			
 			// aapt resource value: 0x7f090094
-			public const int mr_volume_item_icon = 2131296404;
+			public const int mr_media_main_control = 2131296404;
+			
+			// aapt resource value: 0x7f09008f
+			public const int mr_name = 2131296399;
 			
 			// aapt resource value: 0x7f090095
-			public const int mr_volume_slider = 2131296405;
+			public const int mr_playback_control = 2131296405;
+			
+			// aapt resource value: 0x7f09008e
+			public const int mr_title_bar = 2131296398;
+			
+			// aapt resource value: 0x7f090097
+			public const int mr_volume_control = 2131296407;
+			
+			// aapt resource value: 0x7f090098
+			public const int mr_volume_group_list = 2131296408;
+			
+			// aapt resource value: 0x7f09009a
+			public const int mr_volume_item_icon = 2131296410;
+			
+			// aapt resource value: 0x7f09009b
+			public const int mr_volume_slider = 2131296411;
 			
 			// aapt resource value: 0x7f090019
 			public const int multiply = 2131296281;
@@ -3120,14 +3123,14 @@ namespace eRestoran.Mobile.Droid
 			// aapt resource value: 0x7f090010
 			public const int normal = 2131296272;
 			
+			// aapt resource value: 0x7f0900b0
+			public const int notification_background = 2131296432;
+			
 			// aapt resource value: 0x7f0900aa
-			public const int notification_background = 2131296426;
+			public const int notification_main_column = 2131296426;
 			
-			// aapt resource value: 0x7f0900a4
-			public const int notification_main_column = 2131296420;
-			
-			// aapt resource value: 0x7f0900a3
-			public const int notification_main_column_container = 2131296419;
+			// aapt resource value: 0x7f0900a9
+			public const int notification_main_column_container = 2131296425;
 			
 			// aapt resource value: 0x7f090037
 			public const int parallax = 2131296311;
@@ -3150,11 +3153,11 @@ namespace eRestoran.Mobile.Droid
 			// aapt resource value: 0x7f090035
 			public const int right = 2131296309;
 			
-			// aapt resource value: 0x7f0900a9
-			public const int right_icon = 2131296425;
+			// aapt resource value: 0x7f0900af
+			public const int right_icon = 2131296431;
 			
-			// aapt resource value: 0x7f0900a5
-			public const int right_side = 2131296421;
+			// aapt resource value: 0x7f0900ab
+			public const int right_side = 2131296427;
 			
 			// aapt resource value: 0x7f09001a
 			public const int screen = 2131296282;
@@ -3219,8 +3222,8 @@ namespace eRestoran.Mobile.Droid
 			// aapt resource value: 0x7f090017
 			public const int showTitle = 2131296279;
 			
-			// aapt resource value: 0x7f0900b2
-			public const int sliding_tabs = 2131296434;
+			// aapt resource value: 0x7f0900b8
+			public const int sliding_tabs = 2131296440;
 			
 			// aapt resource value: 0x7f090073
 			public const int smallLabel = 2131296371;
@@ -3252,8 +3255,8 @@ namespace eRestoran.Mobile.Droid
 			// aapt resource value: 0x7f090036
 			public const int start = 2131296310;
 			
-			// aapt resource value: 0x7f0900a0
-			public const int status_bar_latest_event_content = 2131296416;
+			// aapt resource value: 0x7f0900a6
+			public const int status_bar_latest_event_content = 2131296422;
 			
 			// aapt resource value: 0x7f09005e
 			public const int submenuarrow = 2131296350;
@@ -3264,17 +3267,23 @@ namespace eRestoran.Mobile.Droid
 			// aapt resource value: 0x7f090011
 			public const int tabMode = 2131296273;
 			
-			// aapt resource value: 0x7f0900b0
-			public const int text = 2131296432;
+			// aapt resource value: 0x7f0900b6
+			public const int text = 2131296438;
 			
-			// aapt resource value: 0x7f0900ae
-			public const int text2 = 2131296430;
+			// aapt resource value: 0x7f0900b4
+			public const int text2 = 2131296436;
+			
+			// aapt resource value: 0x7f090083
+			public const int textInputEditText1 = 2131296387;
 			
 			// aapt resource value: 0x7f090052
 			public const int textSpacerNoButtons = 2131296338;
 			
 			// aapt resource value: 0x7f090051
 			public const int textSpacerNoTitle = 2131296337;
+			
+			// aapt resource value: 0x7f090081
+			public const int textView1 = 2131296385;
 			
 			// aapt resource value: 0x7f090080
 			public const int text_input_password_toggle = 2131296384;
@@ -3285,8 +3294,8 @@ namespace eRestoran.Mobile.Droid
 			// aapt resource value: 0x7f09000d
 			public const int textinput_error = 2131296269;
 			
-			// aapt resource value: 0x7f0900a6
-			public const int time = 2131296422;
+			// aapt resource value: 0x7f0900ac
+			public const int time = 2131296428;
 			
 			// aapt resource value: 0x7f09004a
 			public const int title = 2131296330;
@@ -3297,8 +3306,8 @@ namespace eRestoran.Mobile.Droid
 			// aapt resource value: 0x7f090057
 			public const int title_template = 2131296343;
 			
-			// aapt resource value: 0x7f0900b3
-			public const int toolbar = 2131296435;
+			// aapt resource value: 0x7f0900b9
+			public const int toolbar = 2131296441;
 			
 			// aapt resource value: 0x7f090029
 			public const int top = 2131296297;
@@ -3315,20 +3324,29 @@ namespace eRestoran.Mobile.Droid
 			// aapt resource value: 0x7f09000b
 			public const int transition_scene_layoutid_cache = 2131296267;
 			
+			// aapt resource value: 0x7f090085
+			public const int txePassword = 2131296389;
+			
+			// aapt resource value: 0x7f090084
+			public const int txvPassword = 2131296388;
+			
 			// aapt resource value: 0x7f090009
 			public const int up = 2131296265;
 			
 			// aapt resource value: 0x7f090018
 			public const int useLogo = 2131296280;
 			
+			// aapt resource value: 0x7f090082
+			public const int usernameTxv = 2131296386;
+			
 			// aapt resource value: 0x7f09000e
 			public const int view_offset_helper = 2131296270;
 			
-			// aapt resource value: 0x7f0900b4
-			public const int visible = 2131296436;
+			// aapt resource value: 0x7f0900ba
+			public const int visible = 2131296442;
 			
-			// aapt resource value: 0x7f090093
-			public const int volume_item_container = 2131296403;
+			// aapt resource value: 0x7f090099
+			public const int volume_item_container = 2131296409;
 			
 			// aapt resource value: 0x7f090027
 			public const int withText = 2131296295;
