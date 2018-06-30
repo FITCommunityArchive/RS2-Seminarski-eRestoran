412d02570f133ebe3dd7a1d72244ab7119b84221
Shekey <ajdin_sah@hotmail.com>
2018-06-30 21:16:46 +0200

    Izvjestaji

---
+++ eRestoran.Client.Mobile/eRestoran.Client.Mobile/Views/Izvjestaji.xaml.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace eRestoran.Client.Mobile.Views
+{
+	[XamlCompilation(XamlCompilationOptions.Compile)]
+	public partial class Izvjestaji : ContentPage
+	{
+		public Izvjestaji ()
+		{
+			InitializeComponent ();
+		}
+	}
+}
