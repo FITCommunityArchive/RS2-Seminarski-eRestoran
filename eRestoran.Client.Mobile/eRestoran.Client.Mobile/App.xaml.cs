66fc3f77e0baf24fb4eb9ebb2ebc4c0ec80cbe30
Sheky <ajdin_sah@hotmail.com>
2018-10-02 19:29:32 +0200

    updated xamarin forms and added if statements for android, that couzes errors

--- eRestoran.Client.Mobile/eRestoran.Client.Mobile/App.xaml.cs
+++ eRestoran.Client.Mobile/eRestoran.Client.Mobile/App.xaml.cs
@@ -1,6 +1,8 @@
 ﻿using Xamarin.Forms;
 using eRestoran.Client.Mobile.Navigation;
+#if __ANDROID__
 using Android.Runtime;
+#endif
 using eRestoran.PCL.VM;
 using System.Collections.Generic;
 using eRestoran.Client.Mobile.Helpers;
@@ -16,13 +18,20 @@ namespace eRestoran.Client.Mobile
 		{
 			InitializeComponent();
             MainPage = new Login();
+#if __ANDROID__
+
             AndroidEnvironment.UnhandledExceptionRaiser += HandleAndroidException;
-            
+            // Android-specific code
+#endif
+
         }
+#if __ANDROID__
         void HandleAndroidException(object sender, RaiseThrowableEventArgs e)
         {
             e.Handled = true;
         }
+        // Android-specific code
+#endif
         protected override void OnStart ()
 		{
             var cart = new CartIndexVM();
