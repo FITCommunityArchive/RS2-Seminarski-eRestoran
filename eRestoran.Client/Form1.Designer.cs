d929288c5410e5b4924c92274ee721c2966abca1
Mirza <mirza.ho@hotmail.com>
2019-03-18 00:04:17 +0100

    - added icon

--- eRestoran.Client/Form1.Designer.cs
+++ eRestoran.Client/Form1.Designer.cs
@@ -351,6 +351,7 @@ namespace FastFoodDemo
             this.Controls.Add(this.panel2);
             this.Controls.Add(this.panel1);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.Name = "Form1";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Form1";
